835b97be20558de97e91e6cfb44a9dc508c17549
Beka <beqarioni@yahoo.co.uk>
2024-08-20 14:43:53 +0400

    asdf

--- utils/utilsDom.js/initiateDocumentListeners.js
+++ utils/utilsDom.js/initiateDocumentListeners.js
@@ -4,7 +4,11 @@ export const initiateDocumentListeners = () => {
     const trigger = document.querySelector(".styled-inputs .dropdown-trigger");
     const menu = document.querySelector(".styled-inputs .dropdown-menu");
 
-    if (!trigger.contains(event.target) && !menu.contains(event.target)) {
+    if (
+      trigger &&
+      !trigger.contains(event.target) &&
+      !menu.contains(event.target)
+    ) {
       trigger.setAttribute("aria-expanded", "false");
       menu.style.display = "none";
       menu.setAttribute("aria-hidden", "true");
--- utils/utilsDom.js/initiateProject/initiateDropdown.js
+++ utils/utilsDom.js/initiateProject/initiateDropdown.js
@@ -6,6 +6,7 @@ export const initDropdown = (container) => {
     const expanded = this.getAttribute("aria-expanded") === "true";
 
     this.setAttribute("aria-expanded", !expanded);
+    menu.setAttribute("aria-hidden", false);
     menu.style.display = expanded ? "none" : "block";
   });
 
@@ -13,6 +14,9 @@ export const initDropdown = (container) => {
   const dropdownOptions = Array.from(
     container.querySelectorAll(".dropdown-option")
   );
+
+  console.log(priorityInput[0]);
+
   dropdownOptions.forEach((dropdownOption) => {
     dropdownOption.addEventListener("click", function (e) {
       const dropdownValue = this.dataset.priority;
